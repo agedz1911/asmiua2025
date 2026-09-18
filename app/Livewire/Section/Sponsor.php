57486022a66f2c3d40261ce557e1e667943c9428
AV Pro <asep.cgeds@gmail.com>
2026-09-18 15:28:45 +0800

    feat: add order by no urut in sponsor page

--- app/Livewire/Section/Sponsor.php
+++ app/Livewire/Section/Sponsor.php
@@ -12,7 +12,7 @@ class Sponsor extends Component
 {
     public function render()
     {
-        $sponsors = ModelsSponsor::where('is_active', true)->get();
+        $sponsors = ModelsSponsor::where('is_active', true)->orderBy('no_urut', 'asc')->get();
 
         $groupedSponsors = $sponsors->groupBy('category');
         $orderedCategories = ['Platinum Sponsor', 'Gold Sponsor', 'Silver Sponsor', 'Exhibitors and Scientific Grant'];
